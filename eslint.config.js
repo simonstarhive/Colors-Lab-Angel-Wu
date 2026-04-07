56eb9cca5a78196de1d2d770d2d0d8f12f645594
Simon <simon.starhive@proton.me>
2026-04-07 17:22:14 -0400

    files fixed

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module"
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off"
+    }
+  }
+];
